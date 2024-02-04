33315fbf7f327f63682a39894cb86924a46080e0
Eldaniz Akbarzade <elakba@taltech.ee>
2024-02-04 11:42:37 +0200

    some updates

--- js/bootstrap.bundle.js
+++ js/bootstrap.bundle.js
@@ -104,20 +104,8 @@ init()
 
 
 
-// testimonials slider
 
-$(document).ready(function(){
-  $("#testimonial-slider").owlCarousel({
-      items:1,
-      itemsDesktop:[1000,1],
-      itemsDesktopSmall:[979,1],
-      itemsTablet:[768,1],
-      pagination: true,
-      slideSpeed:5000,
-      transitionStyle:"fade",
-      autoPlay:true
-  });
-});
+
 
 
 
